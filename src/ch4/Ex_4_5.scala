ae4ec253e19375a34bec798daf95af27c4d5c023
Kyungwook Park <parksama@gmail.com>
2016-12-27 17:54:27 +0900

    ch4

---
+++ src/ch4/Ex_4_5.scala
@@ -0,0 +1,25 @@
+package ch4
+
+/**
+  * Created by humphrey.park on 2016. 12. 27..
+  */
+object Ex_4_5 {
+  def main(args: Array[String]): Unit = {
+    val flyingWhale = new Animal
+    flyingWhale.swim
+    flyingWhale.fly
+  }
+
+  class Animal extends Flying with Swimming {
+    //    override def fly: Unit = println("날다 떨어져요~")
+  }
+
+  trait Flying {
+    def fly = println("날아요 날아~")
+  }
+
+  trait Swimming {
+    def swim = println("촤아~ 촤아~ 헤엄칩니다")
+  }
+
+}
